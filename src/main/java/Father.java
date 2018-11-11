de86f5d41fe9b8ffced6a8a166fbd2459cf3e00a
kaina <326288672@qq.com>
2018-11-11 16:50:28 +0800

    新添加address字段

--- src/main/java/Father.java
+++ src/main/java/Father.java
@@ -1,6 +1,7 @@
 public class Father {
     private int id;
     private String name;
+    private String address;
 
     public int getId() {
         return id;
@@ -16,5 +17,13 @@ public class Father {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
